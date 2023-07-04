ddd5a38c0367412446da7f1543e14759cc688440
tranleduyan <leduyantran@gmail.com>
2023-07-04 10:56:30 -0500

    declare styling

---
+++ src/Components/Buttons/StandardButton/StandardButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StandardButton() {
+  return (
+    <div>StandardButton</div>
+  )
+}
+
+export default StandardButton
---
+++ src/Components/InputFields/StandardInputField/StandardInputField.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StandardInputField() {
+  return (
+    <div>StandardInputField</div>
+  )
+}
+
+export default StandardInputField
