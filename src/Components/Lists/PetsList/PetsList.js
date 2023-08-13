018feb61918ebddfa4e8becb8246f3e632a04853
lethienantran <trana20@spu.edu>
2023-08-13 14:48:50 -0700

    change pet list bacl

--- src/Components/Lists/PetsList/PetsList.js
+++ src/Components/Lists/PetsList/PetsList.js
@@ -1,54 +1,63 @@
-/* Components */
-import React, { useState, useEffect } from "react";
-import PetCard from "../../PetCard/PetCard";
-import SimpleBar from "simplebar-react";
-import "simplebar-react/dist/simplebar.min.css";
-import axios from "axios";
-import { connect } from "react-redux";
-import { API } from "../../../Global/Constants";
+//#region Import Components
+import React, { useState, useEffect }from 'react'
+import PetCard from '../../PetCard/PetCard'
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { API } from '../../../Global/Constants';
+//#endregion
 
-/* Stylings */
-import "../../../Styles/Components/Lists/PetsList/PetsList.css";
-
-/* Icons */
+//#region Import Stylings
+import '../../../Styles/Components/Lists/PetsList/PetsList.css'
+//#endregion
 
 function PetsList(props) {
+
+  //#region Variables
+
+  /* API */
+  const apiURL = `${API.petsListURL}${props.userId}`
+  const apiKey = API.key;
+
   /* Data for Pets List */
   const [petsListData, setPetsListData] = useState([]);
 
   /* Message to display in Pets List */
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
-  /* API */
-  const apiURL = `${API.petsListURL}${props.userId}`;
-  const apiKey = API.key;
+  //#endregion
 
-  useEffect(() => {
+  //#region Functions
+  
+  useEffect(()=>{
     /* Get Method */
     axios
       .get(apiURL, {
         headers: {
-          "X-API-KEY": apiKey,
-        },
+          'X-API-KEY': apiKey
+        }
       })
-      .then((response) => {
+      .then(response => {
         /* If success, set message to empty */
-        if (response.status === 200) {
+        if(response.status === 200){
+
           /* Stringify Response to parse for setting data for Pets List*/
           const JSONFormat = JSON.stringify(response.data.responseObject);
           setPetsListData(JSON.parse(JSONFormat));
-          setMessage("");
+          setMessage('');
         }
       })
-
+      
       /* Set error to the message */
-      .catch((error) => {
+      .catch(error => {
         setMessage(error.response.data.message);
-      });
+      })
   }, [apiURL, apiKey]);
 
   /* Calculate Age using date of birth object passed in */
   const CalculateAge = (dob) => {
+    
     /* Convert 'dob' to Date */
     const birthDate = new Date(dob);
 
@@ -62,22 +71,21 @@ function PetsList(props) {
     const monthDifference = today.getMonth() - birthDate.getMonth();
 
     /* If month difference less than 0 or if in the birthday month but have not the birth date yet, then no birthday this year, minus age by 1 */
-    if (
-      monthDifference < 0 ||
-      (monthDifference === 0 && today.getDate() < birthDate.getDate())
-    ) {
+    if(monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())){
       age--;
     }
     return age;
-  };
+  }
 
-  const className = ` petsList-Container ${props.className}`;
+  //#endregion
+
+  const className= ` petsList-Container ${props.className}`;
   return (
     <SimpleBar className={className}>
       {petsListData.length === 0 ? (
-        <p className="petsList-Message paragraph2">{message}</p>
-      ) : (
-        petsListData.map((pet) => (
+          <p className='petsList-Message paragraph2'>{message}</p>
+        ) : (
+          petsListData.map(pet => (
           <PetCard
             key={pet.ppId}
             petAvatar={pet.photo}
@@ -87,14 +95,15 @@ function PetsList(props) {
             petBreed={pet.petBreed}
             ownerAvatar={props.userData.userAvatar}
           />
-        ))
-      )}
+          ))
+        )
+      }
     </SimpleBar>
-  );
+  )
 }
 
 const mapStateToProps = (state) => ({
   userData: state.userData,
 });
 
-export default connect(mapStateToProps)(PetsList);
+export default connect(mapStateToProps)(PetsList)
