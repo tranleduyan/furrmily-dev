4cd41c498bf41bd7ed277d7f6f51ca4efaa0a5c5
tranleduyan <leduyantran@gmail.com>
2023-08-20 23:04:08 -0500

    Connect to redux, populate petprofiles and assignee, and implement addtask functionality

--- src/Components/Modals/AddTaskModal/AddTaskModal.js
+++ src/Components/Modals/AddTaskModal/AddTaskModal.js
@@ -1,25 +1,24 @@
 //#region Import Components
 import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import axios from 'axios'
 import GeneralModal from '../GeneralModal/GeneralModal'
 import StandardButton from '../../Buttons/StandardButton/StandardButton'
 import StandardInputField from '../../InputFields/StandardInputField'
 import StandardDropDown from '../../Dropdowns/StandardDropDown/StandardDropDown'
-import { OPTIONS_DATA } from '../../../Global/Constants'
+import { API, OPTIONS_DATA } from '../../../Global/Constants'
 import { UITEXT } from '../../../Global/Constants'
 import Message from '../../Message/Message'
-import { Helpers } from '../../../Global/Helpers'
+import { Converters, Helpers } from '../../../Global/Helpers'
 //#endregion
 
 //#region Import Stylings
 import '../../../Styles/Components/Modals/AddTaskModal.css'
 //#endregion
 
-function AddTaskModal({open, OnClose}) {
+function AddTaskModal({open, OnClose, petProfiles}) {
     
     //#region Variables
-    
-    /* dummyOption for dropdown options placeholder */
-    const dummyOption = [];
 
     /* Error Handler Variables */
     const [errorMessage, setErrorMessage] = useState('');
@@ -41,16 +40,40 @@ function AddTaskModal({open, OnClose}) {
         taskDueHour: '',
         taskDueMinute: '',
         taskDueXM: '',
-        taskAssignee: '',
+        taskAssigneeIds: [],
     });
     
     /* requestBody for API upon adding */
-    const requestBody = {
+    let requestBody = {
         ppID: '',
         taskName: '',
         taskDescription: '',
         taskDueDate: '',
+        taskAssignees: [],
     }
+
+    //#endregion
+
+    //#region Options for Dropdowns
+    
+    /* petProfileAssignees pool for populating petProfileAssineeOptions - This will be updated upon HandlePetProfileInputChange */
+    const [petProfileAssignees, setPetProfileAssignees] = useState([]);
+
+    /* State Value for Assignee DropDown (Object type) - To access and mofify, change this will only change the value of the dropdown, and it is not update the petInformation, you will need to set petInformation useState to update 'petInformation. */
+    const [selectedPetProfileAssignee, setSelectedPetProfileAssignee] = useState({});
+
+    /* Options for pet name/profile dropdown */
+    const petProfilesOptions = petProfiles ? petProfiles.map(petProfile => ({
+        value: petProfile.ppId,
+        label: petProfile.petName,
+    })) : [];
+
+    /* Options for assignee dropdown - based on the assignees array that is updated upon HandlePetProfileInputChange */
+    const petProfileAssigneeOptions = petProfileAssignees ? petProfileAssignees.map(petProfileAssignee => ({
+        value: petProfileAssignee.memberUserId,
+        label: petProfileAssignee.memberFirstName,
+    })) : [];
+
     //#endregion
 
     //#region Functions
@@ -58,15 +81,42 @@ function AddTaskModal({open, OnClose}) {
     /* HandleInputChange - takes the propertyName of the input to update taskInformation object with the input value */
     const HandleInputChange = (propertyName, inputValue) => {
         if(isError) {
-            setTaskDueDateError(false);
-            setTaskNameError(false);
-            setPetProfileError(false);
-            setTaskDescriptionError(false);
-            setTaskAssigneeError(false);
-            setIsError(false);
-            setErrorMessage('');
+            ClearError();
         }
         setTaskInformation({...taskInformation, [propertyName]: inputValue});
+    }
+
+    //TODO: Handle push, pop assignee for task's multiple assignee
+    /* HandleInputPetProfileChange - takes the inputValue, use it to find the currPetProfile object within petProfiles, set it's petProfileMembers property to
+    petProfileAssinees, then, set selectedPetProfileAssignee (Assignee's Dropdown value) to be the first value of that petProfileMembers list.
+    Then, set the property with the inputValue and taskInformation's taskAssigneeIds to be the array of that petProfileMembers' memberUserId*/
+    const HandleInputPetProfileChange = (propertyName, inputValue) => {
+        if(isError) {
+            ClearError();
+        }
+        if(inputValue){
+            const currPetProfile = petProfiles.find(petProfile => petProfile.ppId === inputValue);
+            setPetProfileAssignees(currPetProfile.petProfileMembers);
+            setSelectedPetProfileAssignee(currPetProfile.petProfileMembers[0]);
+
+            setTaskInformation({
+                ...taskInformation,
+                [propertyName]: inputValue,
+                taskAssigneeIds: [currPetProfile.petProfileMembers[0].memberUserId],
+            });
+        }
+    }
+
+    //TODO: Handle push, pop assignee for task's multiple assignee
+    /* HandleInputAssigneeChange - takes the inputValue and set it to selectedPetProfileAssignee (Assignee's Dropdown value), then, update taskInformation's propertyName (Which is taskAssigneeIds) to the array of that inputValue */
+    const HandleInputAssigneeChange = (propertyName, inputValue) => {
+        if(isError) {
+            ClearError();
+        }
+        if(inputValue){
+            setSelectedPetProfileAssignee(inputValue);
+            setTaskInformation({...taskInformation, [propertyName]: [inputValue]});
+        }
     }
 
     /* IsValid - Check for required fields */
@@ -109,7 +159,7 @@ function AddTaskModal({open, OnClose}) {
             isValid = false;
         }
 
-        if(!taskInformation.taskAssignee){
+        if(!taskInformation.taskAssigneeIds){
             setTaskAssigneeError(true);
             setErrorMessage(UITEXT.EMPTY_FIELD_ERROR);
             isValid = false;
@@ -129,29 +179,79 @@ function AddTaskModal({open, OnClose}) {
 
         return isValid;
     }
-    /* OnAddTask - TODO: Add Task using all the input information and then reload page*/
+
+    /* OnAddTask - Update Request Body to the taskInformation values, then post it to API and reload upon success */
     const OnAddTask = () => {
-        console.log('TODO: ADD TASK IMPLEMENTATION - ', taskInformation);
         if(!IsValid()){
             return;
         }
+        requestBody = {
+            ppId: taskInformation.ppId,
+            taskName: taskInformation.taskName,
+            taskDescription: taskInformation.taskDescription,
+            taskDueDate: Converters.DateTimeConverter(
+                                    Converters.DateTimeStringConverter(
+                                               taskInformation.taskDueYear, 
+                                               taskInformation.taskDueMonth, 
+                                               taskInformation.taskDueDay, 
+                                               taskInformation.taskDueHour, 
+                                               taskInformation.taskDueMinute, 
+                                               taskInformation.taskDueXM)),
+            taskAssignees: taskInformation.taskAssigneeIds,
+        }
+        axios
+            .post(API.createTaskURL, requestBody, {
+                headers:{
+                    'X-API-KEY': API.key,
+                }
+            })
+            .then(response => {
+                window.location.reload();
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     /* OnCloseModal - if there is error, set error to false and clear error message, close the modal. */
     const OnCloseModal = () => {
         if(isError) {
-            setTaskDueDateError(false);
-            setTaskNameError(false);
-            setPetProfileError(false);
-            setTaskDescriptionError(false);
-            setTaskAssigneeError(false);
-            setIsError(false);
-            setErrorMessage('');
+            ClearError();
         }
+        ClearTaskInformation();
         OnClose();
     }
 
+    /* ClearError - set all error properties to false */
+    const ClearError = () => {
+        setTaskDueDateError(false);
+        setTaskNameError(false);
+        setPetProfileError(false);
+        setTaskDescriptionError(false);
+        setTaskAssigneeError(false);
+        setIsError(false);
+        setErrorMessage('');
+    }
+
+    /* ClearTaskInformation - reset all taskInformation related variables to empty */
+    const ClearTaskInformation = () => {
+        setTaskInformation({
+            ppId: '',
+            taskName: '',
+            taskDescription: '',
+            taskDueDay: '',
+            taskDueMonth: '',
+            taskDueYear: '',
+            taskDueHour: '',
+            taskDueMinute: '',
+            taskDueXM: '',
+            taskAssigneeIds: [],
+        })
+        setSelectedPetProfileAssignee([]);
+    }
+
     //#endregion
+
     return (
         <GeneralModal open={open}>
             <div className='flexRowCenter AddTaskModal-container'>
@@ -167,9 +267,8 @@ function AddTaskModal({open, OnClose}) {
                                     id='ppId' 
                                     name='ppId' 
                                     title='Pet Name' 
-                                    placeholder='' 
-                                    options={dummyOption} 
-                                    onChange={HandleInputChange}
+                                    options={petProfilesOptions} 
+                                    onChange={HandleInputPetProfileChange}
                                     error={petProfileError}/>
 
                         {/* Task Name Input Field */}
@@ -287,13 +386,13 @@ function AddTaskModal({open, OnClose}) {
                         </div>
                          {/* Task Assignee Dropdown Field */}
                          <StandardDropDown className='AddTaskModal-taskAssigneeDropDownContainer' 
-                                    htmlFor='taskAssignee' 
-                                    id='taskAssignee' 
-                                    name='taskAssignee' 
+                                    htmlFor='taskAssigneeIds' 
+                                    id='taskAssigneeIds' 
+                                    name='taskAssigneeIds' 
                                     title='Assignee' 
-                                    placeholder='' 
-                                    options={dummyOption} 
-                                    onChange={() => {}}
+                                    value={selectedPetProfileAssignee.memberUserId}
+                                    options={petProfileAssigneeOptions} 
+                                    onChange={HandleInputAssigneeChange}
                                     error={taskAssigneeError}/>
                         {/* Task Status Dropdown Field */}
                         <StandardDropDown className='AddTaskModal-taskStatusDropDownContainer' 
@@ -312,4 +411,8 @@ function AddTaskModal({open, OnClose}) {
     )
 }
 
-export default AddTaskModal;
+const mapStateToProps = state => ({
+    petProfiles: state.petProfiles.data,
+  });
+
+export default connect(mapStateToProps)(AddTaskModal);
