7d9ead9ee9096a07e1676b5c92f61b833b1181b9
tranleduyan <leduyantran@gmail.com>
2023-08-27 11:21:55 -0500

    Implementation

--- src/Pages/Pets/Pets.js
+++ src/Pages/Pets/Pets.js
@@ -1,5 +1,5 @@
 //#region Import Component
-import React from 'react'
+import React, { useState } from 'react'
 import SideNavBar from '../../Components/NavBars/SideNavBar/SideNavBar'
 import { connect } from 'react-redux'
 import IconButton from '../../Components/Buttons/IconButton/IconButton'
@@ -18,6 +18,13 @@ import IconCard from '../../Components/Cards/IconCard/IconCard'
 //#endregion
 
 function Pets({ userData, petProfiles }) {
+
+  const [selectedPet, setSelectedPet] = useState(null);
+
+  const OnPetCardClicked = (petDetails) => {
+    setSelectedPet(petDetails);
+  }
+
   return (
     <div className='wrapper Pets-wrapper'>
       <SideNavBar 
@@ -52,6 +59,7 @@ function Pets({ userData, petProfiles }) {
                 userId={userData.userId}
                 userAvatar={userData.userAvatar}
                 className="Pets-petsListContainer"
+                onClick={OnPetCardClicked}
               />
               <div className="flexRowCenter">
                 <p className='paragraph2'>
@@ -73,55 +81,58 @@ function Pets({ userData, petProfiles }) {
 
             </section>
           </div>
-          <div className='Pets-contentColumn2'>
-            <div className='Pets-sectionHeaderContainer'>
-              <p className='heading2'>Pom</p>
-              <IconButton
-                    className="flexRowCenter Pets-sectionHeaderIconButton"
-                    icon={<FontAwesomeIcon icon={faPenToSquare} />}
-                    onClick={()=>{}}
-              />
-            </div>
-            <div className='flexColumnCenter Pets-contentContainer'>
-              <div className='Pets-petAvatarPlaceholder flexColumnCenter'>
-                <FontAwesomeIcon icon={faPaw}/>
-              </div>
-              <section className='Pets-petDescriptionHeader flexColumnCenter'>
-                <p className='heading4'>Pomeranian</p>
-                <p className='paragraph2'>Dog</p>
-                <p className='paragraph2'>(15 lbs)</p>
-              </section>
-              <section className='Pets-petInformationContainer'>
-                <IconCard className='Pets-petAboutCard' 
-                          icon={faNoteSticky}>
-                  <p className='paragraph2'>About</p>
-                  <div className='Pets-petAboutContainer'>
-                    <p className='paragraph2'>“I'm a radiant ball of fluff, a Pomeranian with a coat as bright as the sun. Yup, that's right, I've got this striking yellow fur that gets everyone's attention. I must say, it suits my lively and vibrant personality pretty well.
-Speaking of favorite things, have you met my besties? There’s Lucas, the dog toy.”</p>
+          {(selectedPet) && 
+            <>
+              <div className='Pets-contentColumn2'>
+                <div className='Pets-sectionHeaderContainer'>
+                  <p className='heading2'>{selectedPet.name}</p>
+                  <IconButton
+                        className="flexRowCenter Pets-sectionHeaderIconButton"
+                        icon={<FontAwesomeIcon icon={faPenToSquare} />}
+                        onClick={()=>{}}
+                  />
+                </div>
+                <div className='flexColumnCenter Pets-contentContainer'>
+                  <div className='Pets-petAvatarPlaceholder flexColumnCenter'>
+                    <FontAwesomeIcon icon={faPaw}/>
                   </div>
-                </IconCard>
-                <IconCard icon={faLocationDot} 
-                          layout={['ColumnCenter']}
-                          className='Pets-petInformationIconCard'>
-                  <p className='paragraph2'>680 W 118th Plz</p>
-                  <p className='paragraph2'>Seattle, WA, 98178</p>
-                </IconCard>
-                <IconCard icon={faGift} 
-                          layout={['ColumnCenter']}
-                          className='Pets-petInformationIconCard'>
-                  <p className='paragraph2'>February 25th, 2021</p>
-                  <p className='paragraph2'>2 years old</p>
-                </IconCard>
-                <IconCard 
-                  icon={faSquarePlus} 
-                  layout={['ColumnCenter']}
-                  type='action' 
-                  className='Pets-petInformationIconCard'>
-                  <p className='paragraph2'>Add More Pet Information</p>
-                </IconCard>
-              </section>
-            </div>
-          </div>
+                  <section className='Pets-petDescriptionHeader flexColumnCenter'>
+                    <p className='heading4'>{selectedPet.breed}</p>
+                    <p className='paragraph2'>{selectedPet.type}</p>
+                    <p className='paragraph2'>({selectedPet.weight} lbs)</p>
+                  </section>
+                  <section className='Pets-petInformationContainer'>
+                    <IconCard className='Pets-petAboutCard' 
+                              icon={faNoteSticky}>
+                      <p className='paragraph2'>About</p>
+                      <div className='Pets-petAboutContainer'>
+                        <p className='paragraph2'>“{selectedPet.about}”</p>
+                      </div>
+                    </IconCard>
+                    <IconCard icon={faLocationDot} 
+                              layout={['ColumnCenter']}
+                              className='Pets-petInformationIconCard'>
+                      <p className='paragraph2'>{selectedPet.physicalAddress1} {(selectedPet.physicalAddress2) ? ',' : ''} {selectedPet.physicalAddress2}</p>
+                      <p className='paragraph2'>{selectedPet.addressCity}, {selectedPet.addressState}, {selectedPet.addressZip5} {(selectedPet.addressZip4) ? '-': ''} {selectedPet.addressZip4}</p>
+                    </IconCard>
+                    <IconCard icon={faGift} 
+                              layout={['ColumnCenter']}
+                              className='Pets-petInformationIconCard'>
+                      <p className='paragraph2'>{selectedPet.dateOfBirth}</p>
+                      <p className='paragraph2'>{selectedPet.age} years old</p>
+                    </IconCard>
+                    <IconCard 
+                      icon={faSquarePlus} 
+                      layout={['ColumnCenter']}
+                      type='action' 
+                      className='Pets-petInformationIconCard'>
+                      <p className='paragraph2'>Add More Pet Information</p>
+                    </IconCard>
+                  </section>
+                </div>
+              </div>
+            </>
+          }
         </div>
       </div>
     </div>
