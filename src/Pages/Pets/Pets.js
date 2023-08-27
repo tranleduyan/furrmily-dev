f04810bbeb126dd437cbd91d479a8e078622ef10
tranleduyan <leduyantran@gmail.com>
2023-08-26 20:55:51 -0500

    Initialize ui layout

--- src/Pages/Pets/Pets.js
+++ src/Pages/Pets/Pets.js
@@ -13,7 +13,8 @@ import '../../Styles/Pages/Pets/Pets.css'
 
 //#region Import Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusCircle, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faPlusCircle, faPaw, faNoteSticky, faLocationDot, faGift, faPenToSquare, faSquarePlus } from '@fortawesome/free-solid-svg-icons'
+import IconCard from '../../Components/Cards/IconCard/IconCard'
 //#endregion
 
 function Pets({ userData, petProfiles }) {
@@ -42,7 +43,7 @@ function Pets({ userData, petProfiles }) {
                   </div>
                 </div>
                 <IconButton
-                  className="flexRowCenter Pets-AddButton"
+                  className="flexRowCenter Pets-sectionHeaderIconButton"
                   icon={<FontAwesomeIcon icon={faPlusCircle} />}
                   onClick={()=>{}}
                 />
@@ -53,7 +54,9 @@ function Pets({ userData, petProfiles }) {
                 className="Pets-petsListContainer"
               />
               <div className="flexRowCenter">
-                <p className='paragraph2'>{`Total: ${petProfiles.length} ${(petProfiles.length > 1) ? 'Pets' : 'Pet'}`}</p>
+                <p className='paragraph2'>
+                  {`Total: ${petProfiles.length} ${(petProfiles.length > 1) ? 'Pets' : 'Pet'}`}
+                </p>
               </div>
             </section>
 
@@ -63,13 +66,61 @@ function Pets({ userData, petProfiles }) {
               <div className="Pets-sectionHeaderContainer">
                 <p className="heading2 Pets-sectionHeaderText">Members</p>
                 <IconButton
-                  className="flexRowCenter Pets-ContinueButton"
-                  icon={<FontAwesomeIcon icon={faChevronRight} />}
+                  className="flexRowCenter Pets-sectionHeaderIconButton"
+                  icon={<FontAwesomeIcon icon={faPlusCircle} />}
                 />
               </div>
 
-              {/* TODO: CONTENT - Friends List */}
             </section>
+          </div>
+          <div className='Pets-contentColumn2'>
+            <div className='Pets-sectionHeaderContainer'>
+              <p className='heading2'>Pom</p>
+              <IconButton
+                    className="flexRowCenter Pets-sectionHeaderIconButton"
+                    icon={<FontAwesomeIcon icon={faPenToSquare} />}
+                    onClick={()=>{}}
+              />
+            </div>
+            <div className='flexColumnCenter Pets-contentContainer'>
+              <div className='Pets-petAvatarPlaceholder flexColumnCenter'>
+                <FontAwesomeIcon icon={faPaw}/>
+              </div>
+              <section className='Pets-petDescriptionHeader flexColumnCenter'>
+                <p className='heading4'>Pomeranian</p>
+                <p className='paragraph2'>Dog</p>
+                <p className='paragraph2'>(15 lbs)</p>
+              </section>
+              <section className='Pets-petInformationContainer'>
+                <IconCard className='Pets-petAboutCard' 
+                          icon={faNoteSticky}>
+                  <p className='paragraph2'>About</p>
+                  <div className='Pets-petAboutContainer'>
+                    <p className='paragraph2'>“I'm a radiant ball of fluff, a Pomeranian with a coat as bright as the sun. Yup, that's right, I've got this striking yellow fur that gets everyone's attention. I must say, it suits my lively and vibrant personality pretty well.
+Speaking of favorite things, have you met my besties? There’s Lucas, the dog toy.”</p>
+                  </div>
+                </IconCard>
+                <IconCard icon={faLocationDot} 
+                          layout={['ColumnCenter']}
+                          className='Pets-petInformationIconCard'>
+                  <p className='paragraph2'>680 W 118th Plz</p>
+                  <p className='paragraph2'>Seattle, WA, 98178</p>
+                </IconCard>
+                <IconCard icon={faGift} 
+                          layout={['ColumnCenter']}
+                          className='Pets-petInformationIconCard'>
+                  <p className='paragraph2'>February 25th, 2021</p>
+                  <p className='paragraph2'>2 years old</p>
+                </IconCard>
+                <IconCard 
+                  icon={faSquarePlus} 
+                  layout={['ColumnCenter']}
+                  type='action' 
+                  className='Pets-petInformationIconCard'>
+                  <p className='paragraph2'>Add More Pet Information</p>
+                </IconCard>
+              </section>
+            </div>
           </div>
         </div>
       </div>
