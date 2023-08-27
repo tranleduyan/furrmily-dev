1eb7c79b7cc739ae63c6bccdcd8fc8a8de3fd2f8
tranleduyan <leduyantran@gmail.com>
2023-08-27 11:18:30 -0500

    update TimeStampConverter and add CalculateAge

--- src/Global/Helpers.js
+++ src/Global/Helpers.js
@@ -64,12 +64,34 @@ const DateTimeStringConverter = (year, month, day, hour = 0, minute = 0, xm = 'A
     return `${year}-${formattedMonth}-${formattedDay}T${formattedHour}:${formattedMinute}:00.000Z`;
 }
 
+const TimeStampConverter = (timeStamp) => {
+    const date = new Date(timeStamp);
+    
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+    const day = date.getUTCDate().toString().padStart(2, '0');
+    const year = date.getUTCFullYear();
+
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes().toString().padStart(2,'0');
+
+    const period = hours < 12 ? 'AM' : 'PM';
+
+    const hours12 = hours % 12;
+
+    const formattedHours = (hours12 === 0 && period === 'PM') ? '12' : hours12;
+
+    const formattedHoursString = formattedHours.toString().padStart(2, '0');
+
+    return `${month}/${day}/${year} @ ${formattedHoursString}:${minutes} ${period}`;
+}
+
 export const Converters = {
     UpperCaseConverter,
     LowerCaseConverter,
     CapitalConverter,
     DateTimeConverter,
     DateTimeStringConverter,
+    TimeStampConverter,
 }
 
 /* Helpers */
@@ -143,31 +165,32 @@ const IsLeapYear = (year) => {
     return false;
 }
 
-const ConvertTimeStamp = (timeStamp) => {
-    const date = new Date(timeStamp);
+/* Calculate Age using date of birth object passed in */
+const CalculateAge = (dob) => {
     
-    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
-    const day = date.getUTCDate().toString().padStart(2, '0');
-    const year = date.getUTCFullYear();
-
-    const hours = date.getUTCHours();
-    const minutes = date.getUTCMinutes().toString().padStart(2,'0');
-
-    const period = hours < 12 ? 'AM' : 'PM';
-
-    const hours12 = hours % 12;
-
-    const formattedHours = (hours12 === 0 && period === 'PM') ? '12' : hours12;
-
-    const formattedHoursString = formattedHours.toString().padStart(2, '0');
-
-    return `${month}/${day}/${year} @ ${formattedHoursString}:${minutes} ${period}`;
-}
+    /* Convert 'dob' to Date */
+    const birthDate = new Date(dob);
+  
+    /* Get today's Date */
+    const today = new Date();
+  
+    /* Calculate age by difference between today's year and birth year */
+    let age = today.getFullYear() - birthDate.getFullYear();
+  
+    /* To calculate if there is a birthday */
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+  
+    /* If month difference less than 0 or if in the birthday month but have not the birth date yet, then no birthday this year, minus age by 1 */
+    if(monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())){
+      age--;
+    }
+    return age;
+  }
 
 export const Helpers = {
     GetAvatarIcon,
     IsValidDate,
     IsLeapYear,
-    ConvertTimeStamp,
+    CalculateAge,
 }
 
