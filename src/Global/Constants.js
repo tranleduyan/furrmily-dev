fdf7057a9dcb43ea7895346fe4b1a61bd42236c7
lethienantran <lethienantran@gmail.com>
2023-08-31 19:55:08 -0700

    Add new API URL & truncateString function

--- src/Global/Constants.js
+++ src/Global/Constants.js
@@ -9,6 +9,7 @@ export const API = {
   petBreedsURL:'/api/pet-profiles/pet-breed',
   addPetURL: '/api/pet-profiles/create',
   getUserTasksListURL: '/api/users/tasks/', //this one will be used to concat with userId
+  getUserFriendsListURL: '/api/friends/', //this one will be used to concat with userId
   createTaskURL: '/api/tasks/create',
 }
 
